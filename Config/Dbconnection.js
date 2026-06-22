105d1f41982486bb3d77a2f0f8a3280c20464eee
John Doe <you@example.com>
2026-06-22 20:33:14 +0500

    correc

--- Config/Dbconnection.js
+++ Config/Dbconnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const connUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ExpenseTracker";
+    const connUri = process.env.DB_URL;
     await mongoose.connect(connUri);
     console.log(`MongoDB Connected: ${connUri}`);
   } catch (err) {
